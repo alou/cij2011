cf1334be919b628f9a346c63c13af77e3812d114
Alou Dolo <aloud7@gmail.com>
2011-06-28 14:01:57 +0000

    messages d'erreurs

--- cij2011/cij/views.py
+++ cij2011/cij/views.py
@@ -2,6 +2,7 @@
 # -*- coding= UTF-8 -*-
 
 from django.core.context_processors import csrf
+from django.utils.datastructures import MultiValueDictKeyError
 from django.shortcuts import (render_to_response,
                               HttpResponseRedirect, redirect)
 from django.core.urlresolvers import reverse
@@ -26,21 +27,33 @@ def registered(request):
 
     if request.method == 'POST':
         form = PamperForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('confirmation'))
-        c.update({'form': form})
+        try:
+            if 'language' in request.POST and 'title' in request.POST and form.is_valid() :
+                form.save()
+                pamper = Pamper.objects.order_by('-id')[0]
+                return HttpResponseRedirect(reverse('confirmation', args=[pamper.id]))
+
+            elif 'title' in request.POST and not 'language' in request.POST:
+                c.update({'title': request.POST['title']})
+            elif 'language' in request.POST and not 'title' in request.POST:
+                c.update({'language': request.POST['language']})
+            else:
+                c.update({'language': request.POST['language'], 'title': request.POST['title']})
+            c.update({'form': form})
+        except MultiValueDictKeyError:
+            c.update({'form': form})
     return render_to_response('registered.html', c)
 
 def confirmation(request, *args, **kwargs):
-	"""
-	"""
-	num = kwargs["num"] or 1
-	c = {}
-	pamper = Pamper.objects.filter(id=num)
-	print pamper
-	c.update(csrf(request))
-	return render_to_response('confirmation.html', c)
+    """
+    """
+    num = kwargs["num"] or 1
+    c = {}
+    pamper = Pamper.objects.filter(id=num)
+    print pamper
+    c.update(csrf(request))
+    c.update({'pamper': pamper})
+    return render_to_response('confirmation.html', c)
 
 def club(request):
     """
